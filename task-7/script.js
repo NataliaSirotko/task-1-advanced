0d098c17ec11766c8f791e4dfd1e8ec1498e8998
malinaNata <kalinka007@tut.by>
2019-04-30 18:03:14 +0300

    refactor function

--- task-7/script.js
+++ task-7/script.js
@@ -1,44 +1,18 @@
-/*let ball = document.querySelector('#ball'),
-    field = document.querySelector('#field');
 
-      
-ball.addEventListener('click', function go() {
-    let floor = field.clientHeight - ball.clientHeight;
 
-    animate({
-        duration: 2000,
-        timing: makeEaseOut(bounce),
-        draw: function(progress) {
-            ball.style.top = floor * progress + 'px';
-        }
-    });
-});
-
-function makeEaseOut(timing) {
-    return function(timeFraction) {
-        return 1 - timing(1 - timeFraction);
-    }
-}
-
-function bounce(timeFraction) {
-    for (var a = 0, b = 1, result; 1; a += b, b /= 2) {
-      if (timeFraction >= (7 - 4 * a) / 11) {
-        return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
-      }
-    }
-} */
-
-let square = document.querySelector('#square'),
+let bird = document.querySelector('#bird'),
     btn = document.querySelector('button');
 
 btn.addEventListener('click', function() {
     animate(function(timePassed) {
-        square.style.left = timePassed / 5 + 'px';
-    }, 2000);
+    bird.style.left = timePassed / 5 + 'px';
+    bird.style.bottom =  timePassed / 5 + 'px';
+  }, 1000);
+
 });
 
 function animate(draw, duration) {
-    let start = perfomance.now();
+    let start = performance.now();
 
     requestAnimationFrame(function animate(time) {
         let timePassed = time - start;
@@ -53,3 +27,16 @@ function animate(draw, duration) {
         }
     });
 }
+// function makeEaseOut(timing) {
+//     return function(timeFraction) {
+//         return 1 - timing(1 - timeFraction);
+//     }
+// }
+
+// function bounce(timeFraction) {
+//     for (var a = 0, b = 1, result; 1; a += b, b /= 2) {
+//       if (timeFraction >= (7 - 4 * a) / 11) {
+//         return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
+//       }
+//     }
+// }
