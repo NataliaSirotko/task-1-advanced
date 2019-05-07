6ba5c4b1eaa48cffcf6a5ac83c4c8e05f40b2331
malinaNata <kalinka007@tut.by>
2019-05-07 18:00:02 +0300

    fix code

--- task-10/script.js
+++ task-10/script.js
@@ -6,7 +6,7 @@ input[0].addEventListener('blur', () => {
 });
 //c input, начинать с 375 ввод
 input[1].addEventListener('input', () => {
-    let elem = input[1].value.replace(/\D/g, '').match(/(375)(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    let elem = input[1].value.replace(/\D/g, '').match(/(\d{3})(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
     input[1].value = !elem[2] ? '+' + elem[1] : '+' + elem[1] + ' (' + elem[2] + (elem[3] ? ') ' + elem[3] : '') + (elem[4] ? '-' + elem[4] : '') + (elem[5] ? '-' + elem[5] : '');
 
 });
